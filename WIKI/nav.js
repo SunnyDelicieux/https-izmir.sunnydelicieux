eb1baa1b2818c8bf395207b22cd7146f70f9f80f
SunnyDelicieux <charloumaeva36@gmail.com>
2026-09-21 11:37:39 +0200

    ajout logo

--- WIKI/nav.js
+++ WIKI/nav.js
@@ -8,17 +8,27 @@ class Header extends HTMLElement {
       <style>
 
 .sidenav {
-  height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 230px; /* Set the width of the sidebar */
-  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-  z-index: 1; /* Stay on top */
-  top: 0; /* Stay at the top */
+  height: 100%;
+  width: 230px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
   left: 0;
   background-color: #E7E9EB;
-  overflow-x: hidden; /* Disable horizontal scroll */
+  overflow-x: hidden;
   padding-top: 20px;
   border-bottom: none;
   border-right: 1px black solid;
+}
+
+.sidenav .imagehere {
+  width: 170px;
+  height: 120px;
+  margin: 0 auto 15px auto;
+  background-image: url('/img/wiki/wikilogo.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
 .sidenav a {
@@ -30,84 +40,64 @@ class Header extends HTMLElement {
   background-color: white;
 }
 
-  .sidenav p {
+.sidenav p {
   padding: 0px 20px;
   text-decoration: none;
-    }
+}
 
-  .sidenav h2 {
+.sidenav h2 {
   margin-top: 0px;
-  padding: 0px 40px 0px 40px;
+  padding: 0px 40px;
   text-decoration: none;
   text-align: justify;
   border-bottom: 0px;
-    }
-
+}
 
 @media screen and (max-width: 1350px) {
-.sidenav {
-  height: 40px;
-  width: 100%; /* Set the width of the sidebar */
-  position: static;
-  z-index: 1; /* Stay on top */
-  overflow-x: scroll;
-  scrollbar-width: none;
-  top: 0; /* Stay at the top */
-  left: 0;
-  background-color: #E7E9EB;
-  white-space: nowrap;
-  padding-top: 10px;
-  align-content: center;
-  border-bottom: 1px black solid;
-  border-right: none;
-    }
+
+  .sidenav {
+    height: 40px;
+    width: 100%;
+    position: static;
+    overflow-x: scroll;
+    scrollbar-width: none;
+    background-color: #E7E9EB;
+    white-space: nowrap;
+    padding-top: 10px;
+    align-content: center;
+    border-bottom: 1px black solid;
+    border-right: none;
+  }
 
   .sidenav a {
-  padding: 3px 10px;
-  margin: 0px;
-  text-decoration: none;
-  display: inline;
-  border: 0px;
-  background-color: #E7E9EB;
-    }
+    padding: 3px 10px;
+    margin: 0px;
+    display: inline;
+    border: 0px;
+    background-color: #E7E9EB;
+  }
 
-.sidenav .imagehere {
-  display: none;
-            }
+  .sidenav .imagehere,
+  .sidenav br,
+  .sidenav p,
+  .sidenav h2,
+  .sidenav hr {
+    display: none;
+  }
 
-.sidenav br {
-  display: none;
-            }
-
-.sidenav p {
-  display: none;
-            }
-
-.sidenav h2 {
-  display: none;
-            }
-
-.sidenav hr {
-  display: none;
-            }
-
-} 
+}
       </style>
 
- <!-- Side navigation -->
-<div class="sidenav">
-<div class="imagehere" style="background-image: url('https://www.independentmediators.co.uk/wp-content/uploads/2016/02/placeholder-image-768x576.jpg')"></div><br>
-<h2>WIKI IZMIR</h2>
-  <a href="index.html">Izmir</a>
-  <a href="/WIKI/autre/clan.html">Clans</a>
-   <a href="/WIKI/autre/chrono.html">Chronologie</a>
-  <a href="/WIKI/fiche/index.html">Personnages</a>
-</div>
-
+      <div class="sidenav">
+        <div class="imagehere"></div>
+        <h2>WIKI IZMIR</h2>
+        <a href="index.html">Izmir</a>
+        <a href="/WIKI/autre/clan.html">Clans</a>
+        <a href="/WIKI/autre/chrono.html">Chronologie</a>
+        <a href="/WIKI/fiche/index.html">Personnages</a>
+      </div>
     `;
   }
 }
 
 customElements.define('header-component', Header);
-
-
